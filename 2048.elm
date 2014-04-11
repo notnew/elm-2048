78a90a60941cd3d47065dd82037f55b549b990ca
notnew <notnew@no.email>
2014-04-11 01:02:55 -0500

    add score keeping
    track and display the score

--- 2048.elm
+++ 2048.elm
@@ -15,16 +15,19 @@ data Tile = Blank | Numbered Int
 isBlank t = case t of
               Blank -> True
               _     -> False
+getNum (Numbered n) = n
 
+type Game = {brd : Board, score : Score}
 type Board = [[Tile]]
+type Score = Int
 
 defaultBoard : Board
-defaultBoard = addTile 2 (Numbered 2) . addTile 4 (Numbered 2)
-                   <| repeat 4 <| repeat 4 <| Blank
--- defaultBoard = [ [Blank, Blank, Blank, Numbered 2]
---                , [Blank, Blank, Numbered 2, Numbered 4]
---                , [Blank, Numbered 345, Blank, Numbered 292]
---                , [Blank, Blank, Blank, Blank]]
+defaultBoard = addTile 2 <| addTile 4 blankBoard
+blankBoard =  repeat 4 <| repeat 4 <| Blank
+-- defaultBoard = [ [Numbered 2, Blank, Blank, Blank]
+--                , [Numbered 2, Blank, Blank, Blank]
+--                , [Numbered 8, Blank, Blank, Blank]
+--                , [Numbered 8, Blank, Blank, Blank]]
 
 slideRow : [Tile] -> [Tile]
 slideRow ts =
@@ -37,24 +40,41 @@ slideRow ts =
                        ns -> ns
     in combine ns ++ blanks
 
-stepBoard : ({x:Int, y:Int}, Int) -> Board -> Board
-stepBoard (kbdArrows, rand) brd =
-    let newTile = (Numbered (if rand `mod` 4 == 0 then 4 else 2))
-        brd' = slide kbdArrows brd
-    in if | brd' == brd -> brd
-          | otherwise   -> addTile rand newTile brd'
+stepGame : ({x:Int, y:Int}, Int) -> Game -> Game
+stepGame ((kbdArrows, rand) as sig) ({brd, score} as g) =
+    let newBrd = slide kbdArrows brd
+        points = sum <| if kbdArrows.y==0
+                        then zipWith rowPoints brd newBrd
+                        else (zipWith rowPoints `on` transpose) brd newBrd
+    in if | newBrd == brd -> g
+          | otherwise -> {g | brd <- addTile rand newBrd
+                            , score <- g.score + points}
 
-addTile : Int -> Tile -> Board -> Board
-addTile i new brd =
-    let countBlanks = foldl (\t sum -> if isBlank t then (1+sum) else sum) 0
-        idx = i `mod` (sum <| map countBlanks brd)
+rowPoints : [Tile] -> [Tile] -> Score
+rowPoints old new =
+    let sort' : [Tile] -> [Tile]
+        sort' ts = sortWith (\(Numbered a) (Numbered b) -> a `compare` b)
+                   <| filter (not . isBlank) ts
+    in case (sort' old, sort' new) of
+         (o::o'::os, n::ns) ->
+             if | getNum o + getNum o' == getNum n ->
+                    getNum n + rowPoints os ns
+                | getNum o == getNum n   -> rowPoints (o'::os) ns
+                | otherwise              ->  -9999999    --error
+         _ -> 0
+
+addTile : Int -> Board -> Board
+addTile rand brd =
+    let newTile = (Numbered (if rand `mod` 4 == 0 then 4 else 2))
+        countBlanks = foldl (\t sum -> if isBlank t then (1+sum) else sum) 0
+        idx = rand `mod` (sum <| map countBlanks brd)
         replace i (ts::tss) = let blanks = countBlanks ts
                               in if i < blanks
                                  then (replaceInRow i ts) :: tss
                                  else ts :: (replace (i-blanks) tss)
         replaceInRow : Int -> [Tile] -> [Tile]
         replaceInRow i (t::ts) = case (i, t) of
-                               (0, Blank) -> new::ts
+                               (0, Blank) -> newTile::ts
                                (i, Blank) -> Blank :: (replaceInRow (i-1) ts)
                                _          -> t :: (replaceInRow i ts)
     in replace idx brd
@@ -80,13 +100,14 @@ showTile t =
                               |> container tileWidth tileHeight middle
                               |> color grey
 
-render : (Int, Int) -> Board -> Element
-render (w,h) board =
+render : (Int, Int) -> Game -> Element
+render (w,h) {brd, score} =
     let -- canv = collage width height forms
         rowToForm row = flow right <| map (frameTile . showTile) row
         frameTile = container frameWidth frameHeight middle
-        grid = map rowToForm board |> flow down |> container width height middle
-    in container w h middle <| color lightCharcoal <| grid
+        grid = map rowToForm brd |> flow down |> container width height middle
+    in container (w-10) (h-10) middle <| color lightCharcoal <|
+         above (text . toText <| "Score: " ++ show score) grid
 
 kbd : Signal {x:Int, y:Int}
 kbd = dropWhen (isEmpty <~ Keyboard.keysDown)  {x=0,y=0} Keyboard.arrows
@@ -95,7 +116,8 @@ event : Signal ({x:Int, y:Int} , Int)
 event = let rangeMax = product <| [1..16] -- factorial of 16
         in lift2 (,) kbd <| Random.range 0 rangeMax kbd
 
-main = render <~ Window.dimensions ~ foldp stepBoard (defaultBoard) event
+main = render <~ Window.dimensions
+               ~ foldp stepGame {brd = defaultBoard, score = 0} event
 
 -- helpers
 transpose : [[a]] -> [[a]]
@@ -105,3 +127,6 @@ transpose mat = let heads xxs = if any isEmpty xxs
                 in case heads mat of
                      [] -> []
                      hs -> hs :: (transpose <| map tail mat)
+
+on : (b -> b -> c) -> (a -> b) -> a -> a -> c
+on f g x y = f (g x) (g y)
