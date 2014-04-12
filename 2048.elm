1d8dfcef0d57df0ca11436f70a56557bec2d863a
notnew <notnew@no.email>
2014-04-12 16:43:42 -0500

    squashed merge of lots of changes
    added:
      score keeping
      game over detection
      restarting the game
      coloring of tiles
      better randomisation
    
    also reorganised some things
    
    Squashed commit of the following:
    
    commit 0c2f299f4fe02e7fd9afb3b4268ea8bcf0e31183
    Author: notnew <notnew@no.email>
    Date:   Sat Apr 12 15:52:30 2014 -0500
    
        improve tile generation
    
        when generating 2 tiles from a single random number, don't always
        generate them to be the same tile (a pair of 2's or a pair of 4's)
    
    commit ab38a01ea3ed64856a645a8c088caf99768c51a4
    Author: notnew <notnew@no.email>
    Date:   Sat Apr 12 15:27:15 2014 -0500
    
        improve display
    
        nicer looking layout for score and restart button
        add start screen
    
    commit 41372cd3e9653b729cf8f88dc886de52456e6686
    Author: notnew <notnew@no.email>
    Date:   Sat Apr 12 14:55:11 2014 -0500
    
        allow restarting of game
    
    commit 40aef8234000bf5ca1ee927b46d8428a17c6fe38
    Merge: 46f9097 7dddbd8
    Author: notnew <notnew@no.email>
    Date:   Sat Apr 12 14:10:01 2014 -0500
    
        Merge branch 'animate' into restarts
    
        mostly for Event type, but rowPoints is also a little cleaner
        dropped all the time based stuff
    
        Conflicts:
        	2048.elm
    
    commit 46f90971487fb7b9fd6daa5270574145c15d9bc8
    Author: notnew <notnew@no.email>
    Date:   Sat Apr 12 13:52:14 2014 -0500
    
        upgrade from elm 0.11 to 0.12
    
        only very minor changes (some names in Text module)
    
    commit b4c20ea8db2deb9ad06537825e8123fa11e29ef3
    Author: notnew <notnew@no.email>
    Date:   Sat Apr 12 10:38:41 2014 -0500
    
        refactor display code
    
    commit 7dddbd8bd842afa00aabdb54ecc912c31f2a2b56
    Author: notnew <notnew@no.email>
    Date:   Fri Apr 11 15:26:12 2014 -0500
    
        add simple animation
    
        color newly spawned tiles green, then change them to normal color
        after a short time
    
    commit 29bd33938b3d605d8f73caf87d69d1a7d02b1102
    Author: notnew <notnew@no.email>
    Date:   Fri Apr 11 14:00:54 2014 -0500
    
        add Event type
    
        to separately track sliding move events (generated by keyboard) and
        clock ticks (for animations)
    
    commit 99ef9aa3933060f490dce17d5b76b51e4a7cd0e9
    Author: notnew <notnew@no.email>
    Date:   Fri Apr 11 13:35:00 2014 -0500
    
        cleanup, organize code better
    
    commit c374c931a99fb5b29955c36869d60e492c6584a3
    Author: notnew <notnew@no.email>
    Date:   Fri Apr 11 13:24:02 2014 -0500
    
        detect game over state
    
        and display game over message
    
    commit 9a25edbf1432614a9c5d223c512b31eacd04152e
    Author: notnew <notnew@no.email>
    Date:   Fri Apr 11 02:08:44 2014 -0500
    
        add color
    
    commit 5513d47292d61ae667b46fc35b1dbf6b4965c95d
    Author: notnew <notnew@no.email>
    Date:   Fri Apr 11 01:15:05 2014 -0500
    
        use ADT to track keyboard directions

--- 2048.elm
+++ 2048.elm
@@ -3,6 +3,10 @@ import Random
 import Text
 import Window
 
+import Graphics.Input (input, clickable)
+
+data Hole = Hole
+
 (width, height) = (400, 400)
 borderWidth = 10
 tileCount = 4
@@ -10,25 +14,48 @@ tileWidth = (width - (tileCount+1)*borderWidth) `div` tileCount
 tileHeight = (height - (tileCount+1)*borderWidth) `div` tileCount
 (frameWidth, frameHeight) = (tileWidth + borderWidth, tileHeight + borderWidth)
 
-data Tile = Blank | Numbered Int
-
-isBlank t = case t of
-              Blank -> True
-              _     -> False
-getNum (Numbered n) = n
-
+-- MODEL
 type Game = {brd : Board, score : Score}
+
 type Board = [[Tile]]
+data Tile = Blank | Numbered Int
 type Score = Int
 
 defaultBoard : Board
-defaultBoard = addTile 2 <| addTile 4 blankBoard
-blankBoard =  repeat 4 <| repeat 4 <| Blank
--- defaultBoard = [ [Numbered 2, Blank, Blank, Blank]
---                , [Numbered 2, Blank, Blank, Blank]
---                , [Numbered 8, Blank, Blank, Blank]
---                , [Numbered 8, Blank, Blank, Blank]]
+defaultBoard =  repeat 4 <| repeat 4 <| Blank
+-- defaultBoard = [ [Numbered 2, Numbered 32, Numbered 64, Blank]
+--                , [Numbered 4, Numbered 512, Numbered 2048, Blank]
+--                , [Numbered 8, Numbered 1024, Numbered 256, Blank]
+--                , [Numbered 16, Numbered 128, Blank, Blank]]
 
+isBlank : Tile -> Bool
+isBlank tile = case tile of Blank -> True
+                            _     -> False
+getNum : Tile -> Int
+getNum (Numbered n) = n
+
+startBoard : Board -> Bool
+startBoard = isEmpty
+
+gameOver : Board -> Bool
+gameOver brd = slide Down brd == slide Up brd &&
+               slide Left brd == slide Right brd &&
+               slide Left brd == brd
+
+rowPoints : [Tile] -> [Tile] -> Score
+rowPoints old new =
+    let sort' : [Tile] -> [Int]
+        sort' ts = filter (not . isBlank) ts |> map getNum |> sort
+        go : [Int] -> [Int] -> Score
+        go olds news  = case (olds, news) of
+           (o::o'::os, n::ns) ->
+               if | o + o' == n -> n + go os ns
+                  | o ==  n     -> go (o'::os) ns
+                  | otherwise   ->  -9999999    --error
+           _ -> 0
+    in case (sort' old, sort' new) of (os, ns) -> go os ns
+
+-- UPDATE
 slideRow : [Tile] -> [Tile]
 slideRow ts =
     let (blanks,ns) = partition isBlank ts
@@ -40,32 +67,41 @@ slideRow ts =
                        ns -> ns
     in combine ns ++ blanks
 
-stepGame : ({x:Int, y:Int}, Int) -> Game -> Game
-stepGame ((kbdArrows, rand) as sig) ({brd, score} as g) =
-    let newBrd = slide kbdArrows brd
-        points = sum <| if kbdArrows.y==0
-                        then zipWith rowPoints brd newBrd
-                        else (zipWith rowPoints `on` transpose) brd newBrd
+slide : Direction -> Board -> Board
+slide dir = case dir of
+   Left  -> map slideRow
+   Right -> map (reverse . slideRow . reverse)
+   Up    -> transpose . map slideRow . transpose
+   Down  -> transpose . map (reverse . slideRow . reverse) . transpose
+   _     -> id
+
+step : Event -> Game -> Game
+step ev = case ev of
+            Slide dir rand -> stepGame (dir, rand)
+            Restart rand   ->
+                let brd = addTile rand <| addTile rand <| defaultBoard
+                in \_ -> {brd = brd, score = 0}
+
+stepGame : (Direction, Int) -> Game -> Game
+stepGame (dir, rand) ({brd, score} as g) =
+    let newBrd = if startBoard brd
+                 then addTile rand  defaultBoard
+                 else slide dir brd
+        trans = case dir of
+                  Up   -> transpose
+                  Down -> transpose
+                  _    -> id
+        points = sum <| (zipWith rowPoints `on` trans) brd newBrd
     in if | newBrd == brd -> g
           | otherwise -> {g | brd <- addTile rand newBrd
-                            , score <- g.score + points}
-
-rowPoints : [Tile] -> [Tile] -> Score
-rowPoints old new =
-    let sort' : [Tile] -> [Tile]
-        sort' ts = sortWith (\(Numbered a) (Numbered b) -> a `compare` b)
-                   <| filter (not . isBlank) ts
-    in case (sort' old, sort' new) of
-         (o::o'::os, n::ns) ->
-             if | getNum o + getNum o' == getNum n ->
-                    getNum n + rowPoints os ns
-                | getNum o == getNum n   -> rowPoints (o'::os) ns
-                | otherwise              ->  -9999999    --error
-         _ -> 0
+                            , score <- score + points}
 
 addTile : Int -> Board -> Board
 addTile rand brd =
-    let newTile = (Numbered (if rand `mod` 4 == 0 then 4 else 2))
+    let newTile = Numbered <| if modPow 19 rand idx < 4 then 4 else 2
+        modPow m base exp = if | exp == 0 -> base `mod` m
+                               | otherwise ->
+                                   (base * modPow m base (exp-1)) `mod` m
         countBlanks = foldl (\t sum -> if isBlank t then (1+sum) else sum) 0
         idx = rand `mod` (sum <| map countBlanks brd)
         replace i (ts::tss) = let blanks = countBlanks ts
@@ -79,45 +115,83 @@ addTile rand brd =
                                _          -> t :: (replaceInRow i ts)
     in replace idx brd
 
-slide : {x:Int, y:Int} -> Board -> Board
-slide {x,y} = case (x,y) of
-   (-1,0) -> map slideRow
-   (1,0)  -> map (reverse . slideRow . reverse)
-   (0,1)  -> transpose . map slideRow . transpose
-   (0,-1) -> transpose . map (reverse . slideRow . reverse) . transpose
-   _      -> id
+-- Input
+data Event = Slide Direction Int | Restart Int
+data Direction = Left | Right | Up | Down | None
 
+eventS : Signal Event
+eventS = let rangeMax = product <| [1..16] -- factorial of 16
+             toRandInt = Random.range 0 rangeMax
+         in merge (Restart <~ toRandInt restartS.signal)
+                <| Slide <~ kbd ~ toRandInt kbd
+
+restartS = input ()
+
+arrowToDir : {x:Int, y:Int} -> Direction
+arrowToDir {x,y} = case (x,y) of
+                     (-1,  0) -> Left
+                     ( 1,  0) -> Right
+                     ( 0, -1) -> Down
+                     ( 0,  1) -> Up
+                     _        -> None
+
+kbd : Signal Direction
+kbd = arrowToDir <~ Keyboard.arrows
+        |> dropWhen (isEmpty <~ Keyboard.keysDown) None
+
+-- Display
 showTile : Tile -> Element
 showTile t =
     let fontHeight n = toFloat tileHeight /
                          (1.5 + toFloat (String.length <| show n) * 0.5)
+        bgColor n = Color (128 + (min 256 <| (n `div` 8) * 16)
+                               - (n `div` 64) * 6 )
+                          (128 + (n `div` 32) * 10)
+                          (128 + (n `div` 128) * 15)
+                          1
     in case t of
          Blank      -> empty |> size tileWidth tileHeight |> color darkGrey
          Numbered n -> show n |> toText
                               |> Text.height (fontHeight n)
                               |> monospace |> Text.color charcoal |> bold
-                              |> text
+                              |> centered
                               |> container tileWidth tileHeight middle
-                              |> color grey
+                              |> color (bgColor n)
+
+showBoard : Board -> Element
+showBoard brd =
+    let rowToForm row = flow right <| map (frameTile . showTile) row
+        frameTile = container frameWidth frameHeight middle
+    in map rowToForm brd |> flow down |> container width height middle
+
+gameOverElement : Element
+gameOverElement = toText "GAME OVER" |> bold |> Text.height 35 |> centered
+                  |> container width height
+                       (midTopAt (relative 0.5) (relative 0.3))
+                  |> color  lightGrey |> opacity 0.7
 
 render : (Int, Int) -> Game -> Element
 render (w,h) {brd, score} =
-    let -- canv = collage width height forms
-        rowToForm row = flow right <| map (frameTile . showTile) row
-        frameTile = container frameWidth frameHeight middle
-        grid = map rowToForm brd |> flow down |> container width height middle
-    in container (w-10) (h-10) middle <| color lightCharcoal <|
-         above (text . toText <| "Score: " ++ show score) grid
-
-kbd : Signal {x:Int, y:Int}
-kbd = dropWhen (isEmpty <~ Keyboard.keysDown)  {x=0,y=0} Keyboard.arrows
-
-event : Signal ({x:Int, y:Int} , Int)
-event = let rangeMax = product <| [1..16] -- factorial of 16
-        in lift2 (,) kbd <| Random.range 0 rangeMax kbd
+    let board = if | startBoard brd ->
+                       plainText "Press Any Arrow Key to Start"
+                           |> container width height middle
+                   | gameOver brd -> layers [showBoard brd, gameOverElement]
+                   | otherwise    -> showBoard brd
+        scoreElem = "Score: " ++ show score |> toText
+                      |> Text.height 25 |> rightAligned
+        restart = clickable restartS.handle () <| layers [
+           toText "Restart" |> Text.height 25 |> centered
+               |> container 100 35 middle
+               |> color lightCharcoal
+           , spacer 100 35]
+        frame = container width (height + 40)
+    in container (w-10) (h-10) middle
+           <| layers [ frame midBottom <| color lightCharcoal board
+                     , frame topRight restart
+                     , frame topLeft scoreElem]
 
 main = render <~ Window.dimensions
-               ~ foldp stepGame {brd = defaultBoard, score = 0} event
+               ~ foldp step {brd = [], score = 0} eventS
 
 -- helpers
 transpose : [[a]] -> [[a]]
